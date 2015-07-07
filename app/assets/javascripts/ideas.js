ef545f11719dace7b958451f12db128239e2b98b
Tess Griffin <tessjgriffin@gmail.com>
2015-07-07 13:04:20 -0600

    Added ajax delete and populated the page with only ajax calls

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -4,10 +4,11 @@ $(document).ready(function() {
     url: 'v1/ideas',
     success: function(ideas) {
       $.each(ideas, function(index, idea){
-        $("#ideas-div").append("<h4>" + idea.title + "</h4> <p>" + idea.body + "</p>")
+        addIdeaToPage(idea);
       })
     }
   })
+
   $("#create-idea").on("click", function(){
       var ideaParams = { idea: {title: $(".idea-title").val(), body: $(".idea-body").val()} }
 
@@ -16,10 +17,60 @@ $(document).ready(function() {
         url: '/ideas',
         data: ideaParams,
         success: function(newIdea) {
-          $("#ideas-div").before("<h4>" + newIdea.title + "</h4> <p>" + newIdea.body + "</p>")
+          addIdeaToTop(newIdea)
           $(".idea-title").val("")
           $(".idea-body").val("")
       }
     })
   })
 })
+
+function deleteIdea(idea) {
+  var idea_id = idea.id
+  var ideaParams = { idea: {id: idea_id} }
+
+  $.ajax({
+    type: 'delete',
+    url: '/ideas/' + idea_id,
+    success: function(deletedIdea) {
+      $('#idea_' + idea_id).hide()
+      $('.delete-' + idea_id).hide()
+    }
+  })
+}
+
+function addIdeaToPage(idea) {
+  $("#ideas-div").append(
+    "<div id='idea_"
+    + idea.id
+    + "'><h4>"
+    + idea.title
+    + "</h4> <p>"
+    + idea.body
+    + "</p></div>")
+  $('<button>Delete</button>')
+    .addClass('btn delete-' + idea.id)
+    .attr('data-id', idea.id)
+    .on('click', function(){
+      deleteIdea(idea)
+    })
+    .appendTo($('#ideas-div'));
+}
+
+function addIdeaToTop(idea) {
+  $("#ideas-div").before(
+    "<div id='idea_"
+    + idea.id
+    + "'><h4>"
+    + idea.title
+    + "</h4> <p>"
+    + idea.body
+    + "</p></div>")
+  $('<button>Delete</button>')
+    .addClass('btn delete-' + idea.id)
+    .attr('data-id', idea.id)
+    .on('click', function(){
+      deleteIdea(idea)
+    })
+    .insertBefore($('#ideas-div'));
+}
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,12 +1,19 @@
 class IdeasController < ApplicationController
   respond_to :json
   def index
+    @ideas = Idea.all
   end
 
   def create
     #respond_with @idea, status: 201, location: ideas_path
     @idea = Idea.new(idea_params)
     @idea.save!
+    respond_with @idea, location: ""
+  end
+
+  def destroy
+    @idea = Idea.find(params["id"])
+    @idea.destroy!
     respond_with @idea, location: ""
   end
 
